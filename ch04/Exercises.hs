555d84541b219c9de7ea966461e0f45dec4d611a
dxtr_85 <dxtr85@gmail.com>
2018-03-13 19:03:02 +0100

    Ch04 more exercises (not done).

--- ch04/Exercises.hs
+++ ch04/Exercises.hs
@@ -1,5 +1,7 @@
 --file: ch04/Exercises.hs
-import Data.Char (isUpper)
+import Data.Char (isUpper, digitToInt, isDigit)
+import Data.Either
+
 safeHead :: [a] -> Maybe a
 
 safeHead (x:_) = Just x
@@ -22,3 +24,33 @@ splitWith splitFunc xs = case break splitFunc xs of
   ([], xs) -> splitWith splitFunc (tail xs)
   (yx, []) -> [yx]
   (yx, zs) -> [yx] ++ splitWith splitFunc (tail zs)
+
+asInt_fold :: String -> Int
+asInt_fold s | length s > 17 = error "String too long!."
+asInt_fold ('-':s) = negate (asInt_fold s)
+asInt_fold s = foldl step 0 s
+  where step acc s | isDigit s = 10 * acc + (digitToInt s)
+                   | otherwise = error "Not a valid Int string!"
+
+type ErrorMessage = String
+asInt_either :: String -> Either ErrorMessage Int
+asInt_either s | length s > 17 = Left "String too long!."
+asInt_either ('-':s) | all isDigit s = Right (negate (asInt_fold s))
+asInt_either s | not (all (isDigit) s) = Left "Not a valid Int string!."
+asInt_either s = Right (asInt_fold s)
+
+
+concat_foldr :: [[a]] -> [a]
+concat_foldr a = foldr (++) [] a
+
+takeWhile_my :: (a -> Bool) -> [a] -> [a]
+takeWhile_my _ [] = []
+takeWhile_my cond (s:ss) | cond s = s : (takeWhile_my cond ss)
+                         | otherwise = []
+
+takeWhile_my2 :: (a -> Bool) -> [a] -> [a]
+takeWhile_my2 cond s = foldr step [] s
+  where step s acc | cond s = (s:acc)
+                   | otherwise = []
+
+groupByMy :: (a -> a -> Bool) -> [a] -> [[a]]
--- ch04/Fold.hs
+++ ch04/Fold.hs
@@ -4,7 +4,38 @@ myFoldl :: (a -> b -> a) -> a -> [b] -> a
 myFoldl step zero (x:xs) = myFoldl step (step zero x) xs
 myFoldl _ zero [] = zero
 
+
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
 
-myFoldr step zero (x:xs) = step x (foldr step zero xs)
+myFoldr step zero (x:xs) = step x (myFoldr step zero xs)
 myFoldr _ zero [] = zero
+
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+
+myFilter p [] = []
+myFilter p (x:xs)
+  | p x = x : myFilter p xs
+  | otherwise = myFilter p xs
+
+myFilter2 p xs = foldr step [] xs
+  where step x ys | p x = x: ys
+                  | otherwise = ys
+
+myMap :: (a -> b) -> [a] -> [b]
+
+myMap f xs = myFoldr step [] xs
+  where step x ys = f x : ys
+
+
+myFoldl2 :: (a -> b -> a) -> a -> [b] -> a
+
+myFoldl2 f z xs = myFoldr step id xs z
+  where step x g a = g (f a x)
+
+identity :: [a] -> [a]
+identity xs = foldr (:) [] xs
+
+append :: [a] -> [a] -> [a]
+
+append xs ys = foldr (:) ys xs
